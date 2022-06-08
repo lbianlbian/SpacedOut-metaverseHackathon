1b2e4c54ab92aea90e74f4ad9df9bb364c252c08
Aniket Bamotra <aniketbamotra@gmail.com>
2022-06-08 12:11:57 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import Nav from "./components/NavBar/Nav";
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
